646ba7fb1fbe623da182039acfcff8c8b823e9d4
varnant <varnant@gmail.com>
2021-12-17 08:34:31 +0530

    Update Calculator1.java
    third commit

--- Calculator1.java
+++ Calculator1.java
@@ -3,5 +3,9 @@ public static void main(String ar[]){
 System.out.println("git with jenkins test");
 System.out.println("test trigger build in jenkins");
 System.out.println("test 2");
+
 }
+  static{
+  System.out.println("test");
+  }
 }
